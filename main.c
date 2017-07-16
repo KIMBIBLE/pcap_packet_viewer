5edccdec1a30de58c0e3acf9f973f014e5a0174a
BB.Kim <sk9935@naver.com>
2017-07-16 09:30:48 -0700

    add : main.c dumpcode

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
-#include <pcap.h>
+#include <pcap/pcap.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <sys/time.h>
+
+
+void printchar(unsigned char c);
+void dumpcode(const unsigned char *buff, int len);
 
 int main(int argc, char *argv[])
-{
+	{
 	pcap_t *handle;			/* Session handle */
 	char *dev;			/* The device to sniff on */
 	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
 	struct bpf_program fp;		/* The compiled filter */
-	char filter_exp[] = "port 23";	/* The filter expression */
+	char filter_exp[] = "";	/* The filter expression */
 	bpf_u_int32 mask;		/* Our netmask */
 	bpf_u_int32 net;		/* Our IP */
-	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	struct pcap_pkthdr *header;	/* The header that pcap gives us */
 	const u_char *packet;		/* The actual packet */
+	unsigned int packet_number = 0;
 
 	/* Define the device */
 	dev = pcap_lookupdev(errbuf);
@@ -41,10 +48,55 @@ int main(int argc, char *argv[])
 		return(2);
 	}
 	/* Grab a packet */
-	packet = pcap_next(handle, &header);
-	/* Print its length */
-	printf("Jacked a packet with length of [%d]\n", header.len);
+	while(pcap_next_ex(handle, &header, &packet) >= 0)
+	{
+		printf("packet Number : %d\n", packet_number);
+		/* Print its length */
+		printf("Jacked a packet with length of [%d]\n", header->len);
+		dumpcode(packet, header->len);
+		packet_number++;
+		puts("");
+	}
 	/* And close the session */
 	pcap_close(handle);
 	return(0);
 }
+
+void printchar(unsigned char c)
+{
+	if(isprint(c))
+		printf("%c", c);
+	else
+		printf(".");
+}
+
+void dumpcode(const unsigned char *buf, int len)
+{
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		printf("%02x ", buf[i]);
+
+		if (i % 16 - 15 == 0)
+		{
+			int j;
+			printf("  ");
+			for (j = i - 15; j <= i; j++)
+				printchar(buf[j]);
+			printf("\n");
+		}
+	}
+
+	if (i % 16 != 0)
+	{
+		int j;
+		int spaces = (len - i + 16 - i % 16) * 3 + 2;
+		for (j = 0; j < spaces; j++)
+			printf(" ");
+
+		for (j = i - i % 16; j < len; j++)
+			printchar(buf[j]);
+	}
+	printf("\n");
+}
+
